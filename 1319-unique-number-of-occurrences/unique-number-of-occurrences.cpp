8fa6d8090c88dec716580a1dac3369685ba7ab60
Aryan-sac <aryansachan825@gmail.com>
2025-10-15 22:01:38 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (57.26%) - LeetSync

--- 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
+++ 1319-unique-number-of-occurrences/unique-number-of-occurrences.cpp
@@ -2,13 +2,19 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
     unordered_map<int,int> mp;
+
+    // strore freq in map
     for(auto el : arr){
         mp[el]++;
     }
 
     for(auto it = mp.begin(); it != mp.end(); ++it){
         int val = it->second;
+
+        // store a num freq and make it 0
         it->second = 0;
+        
+        // the ckeck stored freq in a map
         for(auto it2 = mp.begin(); it2 != mp.end(); ++it2)
             if(it2->second == val) return false;
         it->second = val;
