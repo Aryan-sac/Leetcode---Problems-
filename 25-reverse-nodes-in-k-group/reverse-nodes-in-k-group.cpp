7248e7ae9654d26a32f160c817902db25a470057
Aryan-sac <aryansachan825@gmail.com>
2025-10-28 23:27:09 +0530

    Time: 0 ms (100.00%) | Memory: 16.4 MB (95.93%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.cpp
@@ -11,24 +11,45 @@
 class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        ListNode *temp = head, *prevNode, *NEXT;
+        ListNode *temp = head, *start, *NEXT,*curr, *lastNode, *newhead=NULL,*prevTail;
         int count = 0;
-        while(count < k)
-        {
-            if(temp == NULL) return head;
+        // count nodes
+        while(temp != NULL){
+            count++;
             temp = temp->next;
-            count++;
         }
-        prevNode = reverseKGroup(temp, k);
+
+        if(count < k) return head;
         temp = head;
-        count = 0;
-        while(count < k){
-            NEXT = temp->next;
-            temp->next = prevNode;
-            prevNode = temp;
-            temp = NEXT;
-            count++;
+        while(count >= k){
+            
+            int c = 0;
+            start = temp;
+            while(c < k){
+                if (c == 0) lastNode = start; // this will be tail after reverse
+                temp = temp->next;
+                c++;
+            }
+            c = 0;
+            curr = temp;
+            while(c < k){
+                NEXT = start->next;
+                start -> next = curr;
+                curr = start;
+                start = NEXT;
+                c++;
+            }
+            if(newhead == NULL){
+                newhead = curr;
+            }
+            else
+                prevTail->next = curr;  // connect with previous tail
+
+            // Update previous tail to current group's tail
+            prevTail = lastNode;
+
+            count = count - k;
         }
-        return prevNode;
+        return newhead;
     }
 };
