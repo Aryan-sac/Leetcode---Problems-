bd8ca15efee0a0edee90061e4be0a37970b523d6
Aryan-sac <aryansachan825@gmail.com>
2025-12-02 00:31:03 +0530

    Time: 28 ms (41.46%) | Memory: 139 MB (83.70%) - LeetSync

--- 239-sliding-window-maximum/sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/sliding-window-maximum.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        deque<int> dq; // stores indices
-        vector<int> result;
+        vector<int>res;
+        deque<int>dq;
+        int n = nums.size();
+        
+        for(int i=0; i<n; i++){
 
-        for (int i = 0; i < nums.size(); i++) {
-            // Remove indices out of current window
-            if (!dq.empty() && dq.front() <= i - k) {
+            // checking All Elements stored in dequeue of current window or NOT
+            while(dq.size()>0 && dq.front()<=(i-k))
                 dq.pop_front();
-            }
-
-            // Remove smaller elements from back
-            while (!dq.empty() && nums[dq.back()] < nums[i]) {
+            
+            // Storing max element at front of dequeue
+            while(dq.size()>0 && nums[dq.back()]<=nums[i])
                 dq.pop_back();
-            }
-
+            
             dq.push_back(i);
-
-            // Record max once window is full
-            if (i >= k - 1) {
-                result.push_back(nums[dq.front()]);
-            }
+            
+            if (i >= k - 1)
+                res.push_back(nums[dq.front()]);
         }
-
-        return result;
+        return res;
     }
 };
