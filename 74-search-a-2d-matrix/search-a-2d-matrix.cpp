2d302132643d5ea412a7039a941d4467f51d77fa
Aryan-sac <aryansachan825@gmail.com>
2025-12-21 22:25:51 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (74.73%) - LeetSync

--- 74-search-a-2d-matrix/search-a-2d-matrix.cpp
+++ 74-search-a-2d-matrix/search-a-2d-matrix.cpp
@@ -1,16 +1,36 @@
 class Solution {
 public:
-    bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        int row = 0, col = matrix[0].size()-1;
-        while((row < matrix.size()) && (col >= 0)){
-            if(target == matrix[row][col])
+
+    // Search in row if correct Row found
+    bool srchInRow(vector<vector<int>>& matrix, int target, int row){
+        int st=0, end= matrix[0].size()-1, mid;
+        while(st <= end){
+            mid = st + (end-st)/2;
+            if(matrix[row][mid] == target)
                 return true;
-            else if(target < matrix[row][col])
-                col--;
+            else if (target  < matrix[row][mid])
+                end = mid-1;
             else
-                row++;
+                st = mid+1;
         }
         return false;
+    }    
 
+    // Search a Particular row where target element found
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int st=0, end= matrix.size()-1, mid;
+        while(st <= end){
+            mid = st + (end-st)/2;
+            if(target >= matrix[mid][0] && target <= matrix[mid][matrix[0].size()-1] ){
+                if(srchInRow(matrix, target, mid))
+                    return true;
+                return false;
+            }
+            else if (target  < matrix[mid][0])
+                end = mid-1;
+            else
+                st = mid+1;
+        }
+        return false;
     }
 };
