4de964b6e4f4ec98b136597bd8946347aaec8a4e
Aryan-sac <aryansachan825@gmail.com>
2026-02-01 23:57:50 +0530

    Time: 0 ms (100.00%) | Memory: 11.9 MB (63.28%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.cpp
+++ 14-longest-common-prefix/longest-common-prefix.cpp
@@ -3,20 +3,16 @@ public:
     string longestCommonPrefix(vector<string>& strs) {
         if (strs.empty()) return "";
 
-        // iterate over characters of the first string
         for (int i = 0; i < strs[0].size(); i++) {
 
-            char c = strs[0][i];  // candidate character
-
-            // check this character against all other strings
+            char c = strs[0][i];
             
             for (int j = 1; j < strs.size(); j++) {
-                // mismatch OR string too short
-                if (i >= strs[j].size() || strs[j][i] != c) {
+                if (i > strs[j].size() || strs[j][i] != c) {
                     return strs[0].substr(0, i);
                 }
             }
         }
-        return strs[0]; // if no mismatch, whole first string is prefix
+        return strs[0];
     }
 };
