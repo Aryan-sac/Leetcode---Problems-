25ef07ca340abf804cc3a608244950c970e6654a
Aryan-sac <aryansachan825@gmail.com>
2026-05-03 17:47:24 +0530

    Time: 22 ms (32.50%) | Memory: 26 MB (47.57%) - LeetSync

--- 49-group-anagrams/group-anagrams.cpp
+++ 49-group-anagrams/group-anagrams.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>> mp;
-        for (auto s : strs) {
-            string el = s;
-            sort(el.begin(), el.end());
-            mp[el].push_back(s);
-        }
-        
         vector<vector<string>> res;
-        for (auto i : mp) {
+        unordered_map<string, vector<string>> mp;
+
+        for(auto i: strs){
+            string key = i;
+            sort(key.begin(), key.end());
+            mp[key].push_back(i);  
+        }
+        for(auto i: mp){
             res.push_back(i.second);
         }
         return res;
     }
-
 };
