cfb7267de527ecc63f7c30ca8a5b99fb46b660f5
Aryan-sac <aryansachan825@gmail.com>
2026-05-24 16:11:38 +0530

    Time: 0 ms (100.00%) | Memory: 9.6 MB (96.04%) - LeetSync

--- 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
+++ 232-implement-queue-using-stacks/implement-queue-using-stacks.cpp
@@ -1,53 +1,25 @@
 class MyQueue {
 public:
-    stack<int>st1;
     MyQueue() {
-        
     }
-    
+    queue<int> q1;
+
     void push(int x) {
-        st1.push(x);
+        q1.push(x);
     }
     
     int pop() {
-        int val;
-        stack<int>st2;
-        // storing elements from st1 to st2
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-        // storing (pop value)/(Last value) of st1 stack in st2 
-        val = st2.top();
-        st2.pop();
-        // storing back elements from st2 to st1
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
+        int val = q1.front();
+        q1.pop();
         return val;
     }
     
     int peek() {
-        int val;
-        stack<int>st2;
-        // storing elements from st1 to st2
-        while(!st1.empty()){
-            st2.push(st1.top());
-            st1.pop();
-        }
-        // storing (pop value)/(Last value) of st1 stack in st2 
-        val = st2.top();
-        // storing back elements from st2 to st1
-        while(!st2.empty()){
-            st1.push(st2.top());
-            st2.pop();
-        }
-        return val;
+        return q1.front();
     }
     
     bool empty() {
-        return st1.empty();
+        return q1.empty();
     }
 };
 
