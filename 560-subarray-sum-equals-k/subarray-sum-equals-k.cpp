50bcf90af9bbb333ce757a7025f925dbce8bebbc
Aryan-sac <aryansachan825@gmail.com>
2026-08-04 17:04:01 +0530

    Time: 49 ms (45.15%) | Memory: 45.3 MB (71.09%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -4,9 +4,9 @@ public:
         int count=0;
         int sum=0;
         unordered_map<int, int>mp;
-        mp[0] = 1;
         for(int i=0; i<nums.size(); i++){
             sum += nums[i];
+            if(sum == k) count++;
             if(mp.find(sum-k) != mp.end())
                 count += mp[sum-k];
             
