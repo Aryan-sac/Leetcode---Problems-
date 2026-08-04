aa2f06ad2dfd1acb201f4b22ad57ccc0a29d5ee4
Aryan-sac <aryansachan825@gmail.com>
2026-08-04 22:55:59 +0530

    Time: 86 ms (30.91%) | Memory: 169.5 MB (12.76%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,28 +1,39 @@
 class Solution {
 public:
+    string res = "";
+    int  maxlen = 0;
+    int pal(string s, int l, int r){
+        while(l>=0 && r<s.length() && s[l] == s[r]){
+            l--;
+            r++;
+        }
+        //Length = (r - 1) - (l + 1) + 1 = r - l - 1;
+        return r - l - 1;
+    }
     string longestPalindrome(string s) {
         if (s.empty()) return "";
-        int start = 0, maxLen = 1;
+        int start = 0;
         int n = s.size();
 
-        auto expand = [&](int left, int right) {
-            while (left >= 0 && right < n && s[left] == s[right]) {
-                if (right - left + 1 > maxLen) {
-                    start = left;
-                    maxLen = right - left + 1;
-                }
-                left--;
-                right++;
+        for(int i=0; i<n ; i++){
+            // 1. Check odd-length palindromes (center at i)
+            int len1 = pal(s, i, i);
+            if (len1 > maxlen) {
+                maxlen = len1;
+                // Calculate start index for odd-length palindrome centered at i
+                int start = i - (len1 - 1) / 2;
+                res = s.substr(start, maxlen);
             }
-        };
 
-        for (int i = 0; i < n; i++) {
-            // Odd length palindrome
-            expand(i, i);
-            // Even length palindrome
-            expand(i, i + 1);
+            // 2. Check even-length palindromes (center between i and i + 1)
+            int len2 = pal(s, i, i + 1);
+            if (len2 > maxlen) {
+                maxlen = len2;
+                // Calculate start index for even-length palindrome centered between i and i + 1
+                int start = i - (len2 / 2 - 1);
+                res = s.substr(start, maxlen);
+            }
         }
-
-        return s.substr(start, maxLen);
+        return res;
     }
 };
