cd573e713e198376c8c9e63dd2c7d3d50bf9d8ae
Aryan-sac <aryansachan825@gmail.com>
2026-08-04 12:17:11 +0530

    Time: 3 ms (62.99%) | Memory: 11.6 MB (75.61%) - LeetSync

--- 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
+++ 766-flatten-a-multilevel-doubly-linked-list/flatten-a-multilevel-doubly-linked-list.cpp
@@ -12,34 +12,33 @@ public:
 class Solution {
 public:
     Node* flatten(Node* head) {
-       if(head ==  NULL) return NULL;
-
-        Node *curr = head;
-        while(curr != NULL){
-
-            // if child node exist
-            if(curr->child != NULL){
-
-                // 1. Flatten the Node
-                Node *NEXT  = curr->next;
+        if(head == NULL)
+            return head;
+        Node* curr = head;
+        while(curr!=NULL){
+            if(curr->child != NULL)
+            {
+                // Child node if Possible
+                Node* temp = curr->next;
                 curr->next = flatten(curr->child);
                 curr->next->prev = curr;
                 curr->child = NULL;
 
-                // 2. Finding Tail/(last Node)
-                while(curr->next != NULL)
+                // for TAIL node / Last Node of the Level
+                while(curr->next != NULL){
                     curr = curr->next;
+                }
 
-                // 3. Attach tail with NEXT ptr
-                if(NEXT != NULL)
-                {
-                    curr->next = NEXT;
-                    NEXT->prev = curr;
+                // Attach TAIL node with temp POINTER
+                if(temp != NULL){
+                    curr->next = temp;
+                    temp->prev = curr;
                 }
             }
-
-            curr = curr->next;
+            
+            curr = curr ->next;
+    
         }
-        return head; 
+        return head;
     }
 };
